39e3fe03b6bb1d98d08a623ac8d0509b9004e499
crusher8010 <crusher8010@gmail.com>
2023-02-25 13:53:59 +0530

    added cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:8000"
+    origin: "http://localhost:3000"
 }));
 
 app.use("/", userRoute);
